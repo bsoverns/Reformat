ceb4e33334a0f1173b6e15f1c0ef50d8318c7d2b
Brad <bsoverns@gmail.com>
2022-10-23 15:29:08 -0400

    First Commit

---
+++ Reformat_program/Classes/EncryptionClass.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Reformat_program
+{
+    class EncryptionClass
+    {
+        //private static string Key = "alskd2dcghh5v;d'eoerkkemwertgxzj"; //32 CHARACTERS
+        //private static string IV = "chae3tgffnkldfsk"; //16 CHARACTERS
+        private static string return_data = "";
+
+        public static string Encrypt(string text, string Key, string IV)
+        {
+            try
+            {
+                byte[] plaintextbytes = System.Text.ASCIIEncoding.ASCII.GetBytes(text);
+                AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+                aes.BlockSize = 128;
+                aes.KeySize = 256;
+                aes.Key = System.Text.ASCIIEncoding.ASCII.GetBytes(Key);
+                aes.IV = System.Text.ASCIIEncoding.ASCII.GetBytes(IV);
+                aes.Padding = PaddingMode.PKCS7;
+                aes.Mode = CipherMode.CBC;
+                ICryptoTransform crypto = aes.CreateEncryptor(aes.Key, aes.IV);
+                byte[] encrypted = crypto.TransformFinalBlock(plaintextbytes, 0, plaintextbytes.Length);
+                crypto.Dispose();
+
+                return_data = Convert.ToBase64String(encrypted);
+                return return_data;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return return_data;
+            }
+
+            finally { }
+        }
+
+        public static string Decrypt(string encrypted, string Key, string IV)
+        {
+            try
+            {
+                byte[] encryptedbytes = Convert.FromBase64String(encrypted);
+                AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+                aes.BlockSize = 128;
+                aes.KeySize = 256;
+                aes.Key = System.Text.ASCIIEncoding.ASCII.GetBytes(Key);
+                aes.IV = System.Text.ASCIIEncoding.ASCII.GetBytes(IV);
+                aes.Padding = PaddingMode.PKCS7;
+                aes.Mode = CipherMode.CBC;
+                ICryptoTransform crypto = aes.CreateDecryptor(aes.Key, aes.IV);
+                byte[] secret = crypto.TransformFinalBlock(encryptedbytes, 0, encryptedbytes.Length);
+                crypto.Dispose();
+
+                return_data = System.Text.ASCIIEncoding.ASCII.GetString(secret);
+                return return_data;
+            }
+
+            catch (Exception ex)
+            {
+                return_data = "The password that you entered is incorrect.";
+                return return_data;
+            }
+
+            finally { }
+        }
+    }
+}
---
+++ Reformat_program/Forms/frmMain.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Reformat_program
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.cmbOptions = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbOptions
+            // 
+            this.cmbOptions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOptions.FormattingEnabled = true;
+            this.cmbOptions.Location = new System.Drawing.Point(95, 254);
+            this.cmbOptions.Name = "cmbOptions";
+            this.cmbOptions.Size = new System.Drawing.Size(195, 21);
+            this.cmbOptions.TabIndex = 7;
+            this.cmbOptions.SelectedIndexChanged += new System.EventHandler(this.CheckConvertType);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(14, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(14, 281);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(812, 233);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(14, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(812, 233);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Enabled = false;
+            this.txtPassword.Location = new System.Drawing.Point(476, 255);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(337, 20);
+            this.txtPassword.TabIndex = 8;
+            this.txtPassword.Visible = false;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Enabled = false;
+            this.lblPassword.Location = new System.Drawing.Point(313, 257);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(183, 15);
+            this.lblPassword.TabIndex = 9;
+            this.lblPassword.Text = "Password (Limit - 32 characters)";
+            this.lblPassword.Visible = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 526);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.cmbOptions);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reformat - Paste your list into top box, click convert, copy data from bottom box" +
+    "";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbOptions;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+    }
+}
+
